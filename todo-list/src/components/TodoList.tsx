5c8ec9b299f1598769e1ac4d41c06700854a9c85
Lazaros Georgiadis <paoki200@gmail.com>
2024-02-26 18:58:41 +0200

    Created Delete Button

--- todo-list/src/components/TodoList.tsx
+++ todo-list/src/components/TodoList.tsx
@@ -4,6 +4,10 @@ function TodoList() {
   const [value, setValue] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
 
+  const handleDelete = (index: number) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <form
@@ -23,7 +27,16 @@ function TodoList() {
       </form>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>
+            <a>{todo}</a>
+            <button
+              onClick={() => {
+                handleDelete(index);
+              }}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
